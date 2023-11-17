0b2d6f3e5ed16f0b29fbda15831661302694f6ab
Yann Brillouet <ybrillou@redhat.com>
2023-11-17 09:51:50 +0100

    Migrate namespace to store cache

--- pkg/search/postgres/store_cache.go
+++ pkg/search/postgres/store_cache.go
@@ -309,6 +309,7 @@ func (c *cachedStore[T, PT]) GetAll(ctx context.Context) ([]PT, error) {
 }
 
 func (c *cachedStore[T, PT]) walkCacheNoLock(ctx context.Context, fn func(obj PT) error) error {
+	log.Info("Walking Cache")
 	for _, obj := range c.cache {
 		if !c.isReadAllowed(ctx, obj) {
 			continue
@@ -364,6 +365,7 @@ func (c *cachedStore[T, PT]) isActionAllowed(ctx context.Context, action storage
 			scopeChecker = scopeChecker.ForClusterScopedObject(data)
 		}
 	}
+	log.Infof("IsReadAllowed for %s: %t", c.pkGetter(obj), scopeChecker.IsAllowed())
 	return scopeChecker.IsAllowed()
 }
 
